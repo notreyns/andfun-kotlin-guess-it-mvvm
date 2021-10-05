db7c5ae22d09a7e4f84a275bd1f7a964a0389bbe
Reina <reina.khasanova@iaau.edu.kg>
2021-10-05 09:39:02 +0600

    timer added

--- app/src/main/java/com/example/android/guesstheword/screens/game/GameFragment.kt
+++ app/src/main/java/com/example/android/guesstheword/screens/game/GameFragment.kt
@@ -17,6 +17,7 @@
 package com.example.android.guesstheword.screens.game
 
 import android.os.Bundle
+import android.text.format.DateUtils
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
@@ -64,6 +65,11 @@ class GameFragment : Fragment() {
 
         viewModel.score.observe(viewLifecycleOwner, Observer { newScore ->
             binding.scoreText.text = newScore.toString()
+        })
+
+        viewModel.currenttime.observe(viewLifecycleOwner, Observer{time ->
+            binding.timerText.text = DateUtils.formatElapsedTime(time)
+
         })
 
         viewModel.eventGameFinish.observe(viewLifecycleOwner, Observer{isFinished ->
